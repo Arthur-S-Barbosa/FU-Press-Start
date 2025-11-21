dd68faa147dc1991514580604000cdd36d1eac91
Arthur <163698575+Arthur-S-Barbosa@users.noreply.github.com>
2025-11-21 20:26:29 -0300

    refactor: adjusts errors in index

--- src/index.vue
+++ src/index.vue
@@ -1,51 +1,29 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import startScreen from "./components/startScreen.vue";
 import loginPop from "./components/loginPop.vue";
 
-const screen = ref<"start" | "login">("start");
+const screen = ref("start");
 
 function openLogin() {
     screen.value = "login";
 }
 
 function handleLogin(data) {
-    console.log("🔐 Login frontend:", data);
+    console.log("Login frontend:", data);
     alert("Login enviado!\n(Aqui você conectaria ao backend)");
 }
-// import { ref, onMounted } from 'vue'
-// const text = "Aperte Start";
-// const test = ref("");
-// const click = ref(0);
 
-// const time = ref(true);
-
-// onMounted(() => {
-//     let i = 0;
-//     const interval = setInterval(() => {
-//         test.value += text.charAt(i);
-//         i++;
-//         if (i >= text.length) {
-//             clearInterval(interval);
-//             time.value = false;
-//         }
-//     }, 200);
-//     window.addEventListener('mousemove', (e) => {
-//         x.value = e.clientX
-//         y.value = e.clientY
-//     });
-// });
-
-// function clicou() {
-//     click.value++;
-// }
+onMounted(() => {
+    console.log(screen.value);
+});
 </script>
 
 <template>
     <div class="app bg-fabula">
-        <startScreen v-if="screen === 'start'" @start="openLogin" />
+        <startScreen v-if="screen == 'start'" @start="openLogin" />
 
-        <loginPop v-if="screen === 'login'" @close="screen = 'start'" @login="handleLogin" />
+        <loginPop v-if="screen == 'login'" @close="screen = 'start'" @login="handleLogin" />
     </div>
     <!-- <div class="primary" @click="clicou">
         <h1 class="blur">{{ test }}</h1>
